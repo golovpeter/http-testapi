5adbe4dfc759925572fd29d2e60f3b3a5cfef7e3
Петр Голов <petya-golov@mail.ru>
2022-11-26 00:28:06 +0300

    created http server and added logger

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/golovpeter/http-testapi/internal/app/apiserver"
+	"github.com/joho/godotenv"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+}
+
+func main() {
+	config := apiserver.NewConfig()
+
+	bindAddr := os.Getenv("BIND_ADDR")
+	if bindAddr != "" {
+		config.BindAddr = os.Getenv("BIND_ADDR")
+	}
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel != "" {
+		config.LogLevel = os.Getenv("LOG_LEVEL")
+	}
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatalln(err)
+	}
 }
