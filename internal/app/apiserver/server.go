63811f7817834a18cb4be8ac23362b470710e652
golovpetr <petya-golov@mail.ru>
2022-12-17 20:35:45 +0300

    Added create user endpoint

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,6 +1,8 @@
 package apiserver
 
 import (
+	"encoding/json"
+	"github.com/golovpeter/http-testapi/internal/app/model"
 	"net/http"
 
 	"github.com/golovpeter/http-testapi/internal/store"
@@ -35,7 +37,41 @@ func (s *server) configureRouter() {
 }
 
 func (s *server) HandleUsersCreate() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Email    string `json: email`
+		Password string `json:"password"`
+	}
 
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u := model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+
+		if err := s.store.User().Create(&u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
 	}
 }
